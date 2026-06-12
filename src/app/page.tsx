f5e507856b5eb68e5f2376e334dc2adba1e7c73d
slipanchor578 <slipanchor578@gmail.com>
2026-06-12 15:21:08 +0900

    / = redirect /page/1 が嫌なので、/ はそのまま最新記事5つ掲載するように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,39 @@
-import { redirect } from "next/navigation";
+import { getAllPosts, truncateUnicode } from "@/lib/api";
+import Link from "next/link";
+import Pagination from "./_components/pagination";
 
-export default function Index() {
-  redirect("/page/1");
+export default async function Index() {
+  const page = 1;
+  const perPage = 5;
+
+  const posts = await getAllPosts();
+  const totalPage = Math.ceil(posts.length / perPage);
+
+  const start = (page - 1) * perPage;
+  const end = start + perPage;
+  const pagePosts = posts.slice(start, end);
+
+  return (
+    <>
+      {pagePosts.map((post) => (
+        <article className="post-card" key={post.slug}>
+          <div className="post-content">
+            <h2>
+              <Link href={`/post/${post.slug}`}>{post.title}</Link>
+            </h2>
+            <p className="post-meta">Published on {post.date}</p>
+            <p className="post-excerpt">{truncateUnicode(post.content, 120)}</p>
+            <Link href={`/post/${post.slug}`} className="read-more">
+              Read More
+            </Link>
+          </div>
+        </article>
+      ))}
+
+      <Pagination currentPage={1} totalPage={totalPage} />
+    </>
+  );
 }
-
-// import { getAllPosts, truncateUnicode } from "@/lib/api";
-// import Link from "next/link";
-// export default async function Index() {
-//   const posts = await getAllPosts();
-//   const latest5 = posts.slice(0, 5);
-//   return (
-//     <>
-//       {/* Post Card */}
-//       {latest5.map((post) => (
-//         <article className="post-card" key={post.slug}>
-//           <div className="post-content">
-//             <h2>
-//               <Link href={`/post/${post.slug}`}>{post.title}</Link>
-//             </h2>
-//             <p className="post-meta">Published on {post.date}</p>
-//             <p className="post-excerpt">{truncateUnicode(post.content, 120)}</p>
-//             <Link href={`/post/${post.slug}`} className="read-more">
-//               Read More
-//             </Link>
-//           </div>
-//         </article>
-//       ))}
-//     </>
-//   );
-// }
 
 /*
   {post.date instanceof Date
